bd4fd0e3db6a69e5dea930cccd51eb9244cb1700
Joey Hewitt <joey@joeyhewitt.com>
2014-08-24 21:54:05 -0600

    kgsl: de-inline some things gcc arm-eabi-4.4.3 is complaining about
    Change-Id: I18e09ee171121241e36c0101a7dac7b203b183f4

--- drivers/gpu/msm/adreno_snapshot.c
+++ drivers/gpu/msm/adreno_snapshot.c
@@ -542,7 +542,8 @@ static void ib_parse_type0(struct kgsl_device *device, unsigned int *ptr,
 	}
 }
 
-static inline int parse_ib(struct kgsl_device *device, unsigned int ptbase,
+// Had to de-inline some things to build with gcc arm-eabi-4.4.3
+static /*inline */int parse_ib(struct kgsl_device *device, unsigned int ptbase,
 		unsigned int gpuaddr, unsigned int dwords);
 
 /* Add an IB as a GPU object, but first, parse it to find more goodies within */
@@ -625,7 +626,7 @@ done:
  * access the dynamic data from the sysfs file.  Push all other IBs on the
  * dynamic list
  */
-static inline int parse_ib(struct kgsl_device *device, unsigned int ptbase,
+static /*inline */int parse_ib(struct kgsl_device *device, unsigned int ptbase,
 		unsigned int gpuaddr, unsigned int dwords)
 {
 	unsigned int ib1base, ib2base;
